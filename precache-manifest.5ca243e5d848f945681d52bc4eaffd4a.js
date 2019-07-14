d93cd9d431118e69bbb26285705ce222196f824c
CalebRose <ctrose17@gmail.com>
2019-07-14 13:53:08 -0500

    Updates

---
+++ precache-manifest.5ca243e5d848f945681d52bc4eaffd4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb885c40d801c20101ffd12c418a4246",
+    "url": "/clickBaitGame/index.html"
+  },
+  {
+    "revision": "52ece9d9ed46ef0dfb7a",
+    "url": "/clickBaitGame/static/css/main.36adc0eb.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/clickBaitGame/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "52ece9d9ed46ef0dfb7a",
+    "url": "/clickBaitGame/static/js/main.846b60bf.chunk.js"
+  },
+  {
+    "revision": "3ce7a31920cb62d1a11e",
+    "url": "/clickBaitGame/static/js/runtime~main.78bc2114.js"
+  }
+]);
